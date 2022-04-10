118f2d498ea5d6b4f873b83d38632d4e26d52c23
Daniel P <46326833+moshidev@users.noreply.github.com>
2022-04-10 16:38:02 +0200

    define and start using index_t at MDDChart

--- MDDChart.cpp
+++ MDDChart.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include "MDDChart.hpp"
 
-MDDChart::MDDChart(unsigned n, unsigned m, const std::vector<int>& v) noexcept
+MDDChart::MDDChart(unsigned n, unsigned m, const std::vector<index_t>& v) noexcept
 :n{n}, m{m}, v{v}
 {   }
 
-MDDChart::MDDChart(unsigned n, unsigned m, std::vector<int>&& v) noexcept
+MDDChart::MDDChart(unsigned n, unsigned m, std::vector<index_t>&& v) noexcept
 :n{n}, m{m}, v{v}
 {   }
 
 MDDChart make_MDDChart(std::istream& is, unsigned res) noexcept {
     unsigned n, m;
-    std::vector<int> v;
+    std::vector<MDDChart::index_t> v;
     is >> n >> m;
 
     if (is) {
         v.resize(n*n);
-        for (int i = 0; i < n; i++) {
+        for (unsigned i = 0; i < n; i++) {
             v[i*n+i] = 0;
         }
     }
