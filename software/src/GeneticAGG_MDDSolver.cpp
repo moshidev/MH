8576dff00a0145fada74edb68ad455ce82507c24
Daniel P <46326833+moshidev@users.noreply.github.com>
2022-05-19 18:55:43 +0200

    assure that the best solution is always returned

--- software/src/GeneticAGG_MDDSolver.cpp
+++ software/src/GeneticAGG_MDDSolver.cpp
@@ -17,13 +17,16 @@ MDDSolution GeneticAGG_MDDSolver::solve(unsigned number_of_elements_to_be_chosen
     population = generate_random_population(chromosome_count, number_of_elements_to_be_chosen);
     solution = population[find_best_solution_pos_in_population(population)];
 
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 100'000; i++) {
         population_t selected = select_by_binary_tournament(population, chromosome_count);
         crossover(selected, crossover_probability, crossover_method);
         mutate(selected, mutation_probability);
         reemplace(population, selected);
 
-        solution = population[find_best_solution_pos_in_population(population)];
+        MDDSolution best_in_population = population[find_best_solution_pos_in_population(population)];
+        if (best_in_population.calc_dispersion() < solution.calc_dispersion()) {
+            solution = best_in_population;
+        }
     }
 
     return solution;
