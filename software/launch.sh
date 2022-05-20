56ede9b2bc01a2cf2588c6dde0d9c64fdeac813b
Daniel P <46326833+moshidev@users.noreply.github.com>
2022-05-21 00:05:33 +0200

    update launch script

--- software/launch.sh
+++ software/launch.sh
@@ -17,7 +17,7 @@ function echo_average_time() {
 
 files=$(ls data | sed 's/^/data\//' | sort -V | tr '\n' ',')
 files_wspaces=$(echo $files | tr ',' ' ')
-algorithms="greedy localsearch"
+algorithms="greedy localsearch genetic_agg_uniform genetic_agg_posicion genetic_age_uniform genetic_age_posicion memetic_10_1.0 memetic_10_0.1.dat memetic_10_0.1best.dat"
 seeds=1,2,3,4,5
 
 for a in $algorithms
@@ -38,5 +38,14 @@ do
     done
 done
 
-bin/main_exe greedy "$seeds" "$files" > resultados_greedy.dat
-bin/main_exe localsearch "$seeds" "$files" > resultados_localsearch.dat
+#bin/main_exe greedy "$seeds" "$files" > resultados_greedy.dat
+#bin/main_exe localsearch "$seeds" "$files" > resultados_localsearch.dat
+
+#bin/main_exe genetic_agg_uniform 1 "$files" > resultados_genetic_agg_uniform.dat &
+#bin/main_exe genetic_age_uniform 1 "$files" > resultados_genetic_age_uniform.dat
+#bin/main_exe genetic_agg_position 2 "$files" > resultados_genetic_agg_position_2.dat &
+#bin/main_exe genetic_age_position 1 "$files" > resultados_genetic_age_position.dat
+
+#bin/main_exe memetic_10_0.1best 1 "$files" > resultados_memetic_10_0.1best.dat
+#bin/main_exe memetic_10_0.1 1 "$files" > resultados_memetic_10_0.1.dat &
+#bin/main_exe memetic_10_1.0 1 "$files" > resultados_memetic_10_1.0.dat
